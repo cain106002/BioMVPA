4ce971d2797c1533c1a302de7ffe4c42aa18a6f5
MRI\cain <chenheng_uestc@163.com>
2017-06-12 13:45:15 +0800

    para selection modified

--- mvpa_cross_validation/kfold/cv_kfold.m
+++ mvpa_cross_validation/kfold/cv_kfold.m
@@ -11,27 +11,33 @@ whole_idx = 1:length(label);
 whole_idx = whole_idx';
 label_uniq = unique(label);
 idx_groups = cell(length(label_uniq),1);
-for i = 1:length(label_uniq)
-    idx_groups{i} = whole_idx(label == label_uniq(i),:);
-    idx_groups{i} = idx_groups{i}(randperm(sum(label == label_uniq(i))));
-end
-for i = 1:k
-    train_idx{i} = [];
-    test_idx{i} = [];
-    for j = 1:length(label_uniq)
-        sec_num = floor(sum(label == label_uniq(j))/k);
-        train_idx_tmp = 1:sum(label == label_uniq(j));
-        if(i == k)
-            test_idx_tmp = ((i-1)*sec_num+1):sum(label == label_uniq(j));
-            train_idx_tmp(test_idx_tmp) = [];
-        else
-            test_idx_tmp = ((i-1)*sec_num+1):(i*sec_num);
-            train_idx_tmp(test_idx_tmp) = [];
+
+ses_ind = 0;
+for i_rep = 1:rep
+    for i = 1:length(label_uniq)
+        idx_groups{i} = whole_idx(label == label_uniq(i),:);
+        idx_groups{i} = idx_groups{i}(randperm(sum(label == label_uniq(i))));
+    end
+    for i = 1:k
+        ses_ind = ses_ind+1;
+        train_idx = [];
+        test_idx = [];
+        for j = 1:length(label_uniq)
+            sec_num = floor(sum(label == label_uniq(j))/k);
+            train_idx_tmp = 1:sum(label == label_uniq(j));
+            if(i == k)
+                test_idx_tmp = ((i-1)*sec_num+1):sum(label == label_uniq(j));
+                train_idx_tmp(test_idx_tmp) = [];
+            else
+                test_idx_tmp = ((i-1)*sec_num+1):(i*sec_num);
+                train_idx_tmp(test_idx_tmp) = [];
+            end
+            train_idx = [train_idx;idx_groups{j}(train_idx_tmp)];
+            test_idx = [test_idx;idx_groups{j}(test_idx_tmp)];
         end
-        train_idx{i} = [train_idx{i};idx_groups{j}(train_idx_tmp)];
-        test_idx{i} = [test_idx{i};idx_groups{j}(test_idx_tmp)];
+        mvpa_session(1).idx_train{ses_ind} = train_idx;
+        mvpa_session(1).idx_test{ses_ind} = test_idx;
     end
 end
-        
     
     
--- mvpa_feature_selection/Fscore/fs_Fscore.m
+++ mvpa_feature_selection/Fscore/fs_Fscore.m
@@ -1,4 +1,4 @@
-function [data_f,apply_param,idx_weight] = mvpa_feature_selection(data,label,param)
+function [data_f,apply_param,fs_weight] = fs_Fscore(data,label,para)
 % input:
 % data: 输入数据
 % label: 数据标签
@@ -8,16 +8,25 @@ function [data_f,apply_param,idx_weight] = mvpa_feature_selection(data,label,par
 % apply_param: 输入到mvpa_feature_selection_apply函数中的变换参数,根据该参数进行测试集的特征选择
 % idx_weight: 每个特征的权重，根据这个得到一致特征。
 
-fnum = param(1);
+%% fs_para modification
+fnum = fix(para);
+if(fnum<1)
+    fnum = 1;
+elseif(fnum>size(data,2))
+    fnum = size(data,2);
+end
+
+%% label modification
 label_fs = label(:,1);
 label_unique = unique(label_fs);
 for i = 1:length(label_unique)
     label_fs(label == label_unique(i)) = i;
 end
 
+%% call fsFisher.m
 out = fsFisher(data,label_fs);
-idx_weight = zeros(size(data,2),1);
-idx_weight(out.fList(1:fnum)) = out.W(out.fList(1:fnum));
+fs_weight = zeros(size(data,2),1);
+fs_weight(out.fList(1:fnum)) = out.W(out.fList(1:fnum));
 apply_param = out.fList(1:fnum);
 data_f = data(:,apply_param);
 end
